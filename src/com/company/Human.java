d976b84372f2ae38fa34fd4706e119f3ebcb9d3a
Michał Tomczak <ryba971@gmail.com>
2022-03-05 16:51:10 +0100

    Zadanie 3 i 4: get your salary & give yourself a rise
    Ustawienie gettera i settera dla klasy Human wypisujący info o wypłacie

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,15 +1,50 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class Human {
     public String firstName;
     public String lastName;
-
     public Animal pet;
     public Car car;
+
+    private LocalDateTime lastDate;
+    private Double salary;
+    private Double prevSalary;
+
 
     Human(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
 
     }
+
+    public Double getSalary() {
+        if (lastDate != null) {
+            long diff = ChronoUnit.SECONDS.between(lastDate, LocalDateTime.now());
+            System.out.println("Dane o twojej wypłacie zostały pobrane: " + diff + " sekund temu");
+            System.out.println("Twoja ostatnia wypłata przy sprawdzeniu wynosiła " + prevSalary);
+            System.out.println("Twoja przyszła wypłata to: " + salary);
+        }
+        lastDate = LocalDateTime.now();
+        prevSalary = salary;
+        return salary;
+
+    }
+
+    public void setSalary(Double salary) {
+        if (salary >= 0) {
+            System.out.println("Nowe dane zostały wysłane do systemu księgowego, proszę czekać...");
+            System.out.println("Twój aneks do umowy jest gotowy u Pani Hani z działu kadr");
+            System.out.println("Zmiany w twojej umowie zostały wysłane do ZUS i US");
+            this.salary = salary;
+            System.out.println("Twoja przyszła wypłata będzie wynosić: " + salary);
+        } else {
+            System.out.println("Saldo nie może być ujemne!");
+        }
+
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,25 @@
 package com.company;
 
+import static java.lang.Thread.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         Human me = new Human("Michal", "Tomczak");
-        me.pet = new Animal("york","Azor" );
-        me.pet.feed();
-        me.pet.takeForAWalk();
-        me.pet.takeForAWalk();
-        me.pet.feed();
-        me.car = new Car("a3", "audi",2006);
+//        me.pet = new Animal("york","Azor" );
+//        me.pet.feed();
+//        me.pet.takeForAWalk();
+//        me.pet.takeForAWalk();
+//        me.pet.feed();
+//        me.car = new Car("a3", "audi",2006);
+        me.setSalary(1500.50);
+        me.getSalary();
+        sleep(5000);
+        me.getSalary();
+        me.setSalary(1700.00);
+        me.getSalary();
 
 
 
